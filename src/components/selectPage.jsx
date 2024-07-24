2aa3f42263169f0b1e3cd12a73eb0b25f41addae
David Fischer <fiscd@seas.upenn.edu>
2024-07-24 19:18:16 -0400

    selection

--- src/components/selectPage.jsx
+++ src/components/selectPage.jsx
@@ -6,8 +6,13 @@ export default function StartRaven() {
 
     const navigate = useNavigate();
 
-    const submitContinueClick = () => {
-      navigate("/selectPage")
+
+    const submitStart1Click = () => {
+        navigate("/")
+    }
+
+    const submitStart2Click = () => {
+        navigate("/")
     }
 
     return (
@@ -18,14 +23,36 @@ export default function StartRaven() {
                 <p><h2>SELECT A LAB SCENARIO TO EXPLORE! YOU WILL EXAMINE
                     YOUR CHOSEN CASE STUDY FOR POTENTIAL RISKS OF UN-MASKING.</h2></p>
             </div>
-            
-            <input
-                type="button"
-                onClick={submitContinueClick}
-                value="CONTINUE" />
+
+            <div className="boxes">
+                <div className="box yellow">
+                    <h2>Clinical Trial for a new drug</h2>
+                    <p><h3>A pharmaceutical company is conducting
+                        a phase II clinical trial to evaluate the safety
+                        and efficiacy of a novel drug for treating
+                        hypertension The drug aims to lower blood
+                        pressure without significant side effects.</h3></p>
+                    <input
+                        type="button"
+                        onClick={submitStart1Click}
+                        value="START" />
+                </div>
+                <div className="box blue">
+                    <h2>Behavioral Study on Sleep Patterns</h2>
+                    <p><h3>A psychology research team is investigating the
+                        impact of blue light exposure on sleep quality. Participants
+                        are exposed to different light conditions before bedtime.</h3></p>
+                    <input
+                        type="button"
+                        onClick={submitStart2Click}
+                        value="START" />
+                </div>
+            </div>
+
             <div>
                 <Raven4 />
             </div>
+
         </>
     )
 }
