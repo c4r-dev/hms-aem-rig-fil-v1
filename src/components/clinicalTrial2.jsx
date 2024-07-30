e815eef37e398a1e5e698743acd1a262e085226d
David Fischer <fiscd@seas.upenn.edu>
2024-07-30 14:12:53 -0400

    additional box

--- src/components/clinicalTrial2.jsx
+++ src/components/clinicalTrial2.jsx
@@ -33,9 +33,35 @@ export default function ClinicalTrial2() {
             <div className="ctbackground">
                 <div ref={printRef} style={{ padding: '10px', border: '1px solid #000' }}>
                     <div className="boxes">
-                        <div className="big-box yellow">
+                        <div className="slim-box yellow">
                             <div>
-                                <h1 className="textLeft">RIGOR FILES</h1></div>
+                                <h1 className="textLeft">RIGOR FILES</h1>
+                            </div>
+                            <div>
+                                <h3>CLINICAL TRIAL FOR A NEW DRUG</h3>
+                            </div>
+                            <h4>A pharmaceutical company is conducting
+                                a phase II clinical trial to evaluate the safety
+                                and efficiacy of a novel drug for treating
+                                hypertension The drug aims to lower blood
+                                pressure without significant side effects.</h4>
+                            <div><h3>STUDY TEAM</h3></div>
+                            <div>Principal Investigator (PI): Dr. Emily Carter</div>
+                            <div>Research Coordinator: Sarah Patel</div>
+                            <div>Data Analyst: Dr. Mark Johnson</div>
+                            <div><h3>DATA ACCUMULATION</h3></div>
+                            <div>Blood pressure measurements (systolic and diastolic) at baselline, weekly intervals, and study endpoint</div>
+                            <div>Adverse events reporting</div>
+                            <div>Laboratory tests (e.g., kidney function, liver enzymes)</div>
+                            <div><h3>PARTICIPANTS</h3></div>
+                            <div>500 adult participants (aged 40-65) with diagnosed hypertension.</div>
+                            <div>Randomly assigned to either the experimental drug group or a placebo group.</div>
+                            <div><h3>ENVIRONMENTAL CONDITIONS</h3></div>
+                            <div>Clinical research center with controlled temperature, humidity, and lighting.</div>
+                            <div>Standardized examination rooms.</div>
+                            <div>Compliance with Good Clinical Practice (GCP) guidelines.</div>
+                        </div>
+                        <div className="big-box yellow">
                             <div>
                                 <h2>CAPTURE IDEAS AND QUESTIONS</h2>
                             </div>
